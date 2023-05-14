95f6d949e96abedc81a5605944a1addd9294563b
Jongwon Kim <jkjongwon@gmail.com>
2023-05-14 16:50:43 +0900

    생성작업

---
+++ WWWfit/src/main/java/com/ssafy/wwwfit/WwWfitApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.wwwfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwWfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwWfitApplication.class, args);
+	}
+
+}
