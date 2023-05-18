e13022b65f1eb9a239f957ac4d99586603f25314
JiwonChoi <wldnjs9515@naver.com>
2023-05-18 17:57:47 +0900

    vuex modules

--- frontwwwfit/src/store/index.js
+++ frontwwwfit/src/store/index.js
@@ -1,93 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from "axios";
-import jwtDecode from "jwt-decode";
-import router from "../router";
+import MypageIndex from "@/store/modules/MypageIndex.js";
+import UserIndex from "./modules/UserIndex";
 
 Vue.use(Vuex);
 
-const REST_API = `http://localhost:9999/api-`;
-export default new Vuex.Store({
-  state: {
-    user: {},
-    users: [],
-    loginUser: null,
+const store = new Vuex.Store({
+  modules: {
+    MypageIndex,
+    UserIndex,
   },
-  getters: {},
-  mutations: {
-    LOGIN_USER(state, payload) {
-      let user = {
-        userNo: payload["login-token"].userNo,
-        userId: payload["login-token"].userId,
-        userName: payload["login-token"].userName,
-      };
-      console.log(user);
-      state.loginUser = user;
-    },
-    LOGOUT(state) {
-      state.loginUser = null;
-    },
-  },
-  actions: {
-    registUser({ commit }, user) {
-      const API_URL = `${REST_API}user/user`;
-      axios({
-        url: API_URL,
-        method: "POST",
-        params: user,
-      })
-        .then((res) => {
-          console.log(res);
-          router.push("/login");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-
-      commit;
-    },
-    loginUser({ commit }, user) {
-      // commit;
-      const API_URL = `${REST_API}user/login`;
-      // const API_URL="http://localhost:9999/api-user/login"
-
-      axios({
-        url: API_URL,
-        method: "POST",
-        params: user,
-      })
-        .then((res) => {
-          // debugger;
-          // console.log(res.data);
-
-          const decoded = jwtDecode(res.data["login-token"]);
-          // console.log(decoded);
-
-          if (decoded["login-token"].auth == "true") {
-            sessionStorage.setItem("login-token", res.data["login-token"]);
-            localStorage.setItem(
-              "console msg",
-              // jwtDecode(res.data["login-token"].auth)
-              // decoded["login-token"].auth
-              "no"
-            );
-            commit("LOGIN_USER", decoded);
-            alert("로그인 성공");
-            router.replace({ name: "home" });
-            // router.push("/regist");
-          } else {
-            alert("로그인 실패, 입력을 다시 확인해주세요");
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    logout({ commit }) {
-      sessionStorage.removeItem("login-token");
-      commit("LOGOUT");
-      // router.replace("/");
-    },
-  },
-  modules: {},
 });
+
+export default store;
--- frontwwwfit/src/views/MypageView.vue
+++ frontwwwfit/src/views/MypageView.vue
@@ -1,9 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <div b-container fluid="md">
+      <calendar-view></calendar-view>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import CalendarView from "@/components/mypage/CalendarView.vue";
+// import { mapState } from "vuex";
+export default {
+  components: {
+    CalendarView,
+  },
+};
 </script>
 
 <style></style>
